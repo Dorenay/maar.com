5efaafbf48f7f49e7a4e99564acef1df0d8cfb5d
dorenay <dorenay@gmail.com>
2019-01-22 17:44:18 +0200

    dev by

---
+++ wp-content/themes/main-theme/developed_by.php
@@ -0,0 +1,5 @@
+
+<div style="" class="compas_link"> 
+    <span><img src="https://compas.agency/img/favicon/favicon.ico" alt="COMPAS Agency"></span> 
+    <p style="color: #fff!important;" class="animation_text"> Developed by: <br> <a style="color:#fff!important;" href="https://compas.agency/">COMPAS Agency </a></p>   
+</div>
--- wp-content/themes/main-theme/footer.php
+++ wp-content/themes/main-theme/footer.php
@@ -22,6 +22,7 @@
 					<?php
 				}?>
 			<p class="contact-maar24__copyright">Copyright © 2018. All rights reserved.</p>
+			<?php include('developed_by.php');?>
 		</div>
 		<div class="contact-maar24__maps">
 				<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10295.239733957973!2d23.98903559855387!3d49.827237390612474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smaps!5e0!3m2!1sru!2sua!4v1547731857057" frameborder="0" style="border:0" allowfullscreen></iframe>
