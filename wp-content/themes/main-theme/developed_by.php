748f9e97502b2a1608bc36a8e0005c2e0c310fde
dorenay <dorenay@gmail.com>
2019-01-22 17:47:05 +0200

    dev by fix

--- wp-content/themes/main-theme/developed_by.php
+++ wp-content/themes/main-theme/developed_by.php
@@ -1,5 +1,5 @@
 
 <div style="" class="compas_link"> 
     <span><img src="https://compas.agency/img/favicon/favicon.ico" alt="COMPAS Agency"></span> 
-    <p style="color: #fff!important;" class="animation_text"> Developed by: <br> <a style="color:#fff!important;" href="https://compas.agency/">COMPAS Agency </a></p>   
+    <p class="animation_text"> Developed by: <a href="https://compas.agency/">COMPAS Agency </a></p>   
 </div>
