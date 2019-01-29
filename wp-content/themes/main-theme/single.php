9bdcc7aa4e832078247daa3a4886909205367d72
dorenay <dorenay@gmail.com>
2019-01-29 14:09:16 +0200

    blog fix 4.0

--- wp-content/themes/main-theme/single.php
+++ wp-content/themes/main-theme/single.php
@@ -5,13 +5,14 @@
 ?>
 
     <div class="container">
-    <?php
-    if (have_posts() ) : 
-        while ( have_posts() ) : the_post(); 
-            the_content();
-        endwhile;
-    endif;?>
-    
+        <div class="post__list">
+        <?php
+            if (have_posts() ) : 
+                while ( have_posts() ) : the_post(); 
+                    the_content();
+                endwhile;
+            endif;?>
+        </div>
     </div>
 <?php
     
