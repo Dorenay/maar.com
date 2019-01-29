a233ce8e21530a7c6679f19b612e90912a4daf64
dorenay <dorenay@gmail.com>
2019-01-29 13:32:19 +0200

    blog fix 1.4

--- wp-content/themes/main-theme/blog.php
+++ wp-content/themes/main-theme/blog.php
@@ -3,33 +3,36 @@
     get_header();
     include('page-title.php');
 
-    $posts = get_posts( array(
+    $args = get_posts( array(
         'numberposts' => -1,
         'orderby'     => 'date',
         'order'       => 'DESC',
         'post_type'   => 'post'
     ) );
+    $query = new WP_Query( $args );
 ?>
     <div class="container">
         <div class="post__list">
         <?php 
-            if (have_posts() ) : 
-                while ( have_posts() ) : the_post(); 
-                ?>
-                <a class="post__item" href="<?php the_permalink();?>">
-                    <div class="post__thumbnail">
-                        <?php the_post_thumbnail('post_thumbnail'); ?> 
-                    </div>
-                    <div class="post__body">
-                        <h2><?php the_title(); ?></h2>
-                        <?php
-                            the_excerpt();                            
-                        ?>
-                    </div>
-                </div>
-                <?
-                endwhile;
-            endif;?>
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) { 
+                $query->the_post(); ?>
+                    <a class="post__item" href="<?php the_permalink();?>">
+                        <div class="post__thumbnail">
+                            <?php the_post_thumbnail(); ?> 
+                        </div>
+                        <div class="post__body">
+                            <h2><?php the_title(); ?></h2>
+                            <?php
+                                the_content();
+                                the_excerpt();                            
+                            ?>
+                        </div>
+                    </a><?php
+                }
+                wp_reset_postdata();
+            }
+        ?>
         </div>
     </div>
 <?php
